b1231fae0b40f940c43b91d0bf7f36e352c24c6f
DenchyChiang <dengqing.jiang@drgou.com>
2021-01-27 15:09:38 +0800

    OpenOrderQuery and time format

--- src/main/java/com/holyw/duoduoke/template/jingdong/JingdongGoodsTemplate.java
+++ src/main/java/com/holyw/duoduoke/template/jingdong/JingdongGoodsTemplate.java
@@ -3,8 +3,9 @@ package com.holyw.duoduoke.template.jingdong;
 import com.holyw.duoduoke.support.jingdong.JingdongSupportRepository;
 import com.holyw.duoduoke.utils.jingdong.JingdongGoodsTransUtils;
 import com.holyw.duoduoke.vo.GoodsVO;
-import jd.union.open.goods.query.request.UnionOpenGoodsQueryRequest;
+import com.jd.open.api.sdk.request.AbstractRequest;
 import jd.union.open.goods.query.response.UnionOpenGoodsQueryResponse;
+import jd.union.open.order.query.response.UnionOpenOrderQueryResponse;
 
 
 public class JingdongGoodsTemplate {
@@ -20,7 +21,7 @@ public class JingdongGoodsTemplate {
     /**
      * 详情
      */
-    public GoodsVO getGoodsDetail(UnionOpenGoodsQueryRequest request) {
+    public GoodsVO getGoodsDetail(AbstractRequest request) {
         GoodsVO goodsVO = null;
         try {
             UnionOpenGoodsQueryResponse unionOpenGoodsQueryResponse = jingdongSupportRepository.unionOpenGoodsQuery(request);
@@ -33,5 +34,8 @@ public class JingdongGoodsTemplate {
         return goodsVO;
     }
 
-
+    public UnionOpenOrderQueryResponse orderQuery(AbstractRequest request) {
+        UnionOpenOrderQueryResponse unionOpenOrderQueryResponse = jingdongSupportRepository.unionOpenOrderQuery(request);
+        return unionOpenOrderQueryResponse;
+    }
 }
